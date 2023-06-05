be1b88a3f7e23b9f78c2af26323d85afc1a204a4
rherault <romain@rherault.dev>
2023-06-05 09:49:10 +0000

    feat: new and sort functions

--- card_test.go
+++ card_test.go
@@ -1,6 +1,9 @@
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleCard() {
 	fmt.Println(Card{Rank: Ace, Suit: Heart})
@@ -15,4 +18,46 @@ func ExampleCard() {
 	// Nine of Diamonds
 	// Jack of Clubs
 	// Joker
+}
+
+func TestNew(t *testing.T) {
+	cards := New()
+	// 13 ranks * 4 suits
+	if len(cards) != 13*4 {
+		t.Error("Wrong number of cards in a new deck.")
+	}
+
+	// 4 of each rank
+	for i := range suits {
+		for j := minRank; j <= maxRank; j++ {
+			found := false
+
+			for _, c := range cards {
+				if c.Suit == suits[i] && c.Rank == j {
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				t.Errorf("Card not found: %v of %ss", j, suits[i])
+			}
+		}
+	}
+}
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expected Ace of Spades as first card. Received: ", cards[0])
+	}
+}
+
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expected Ace of Spades as first card. Received: ", cards[0])
+	}
 }
