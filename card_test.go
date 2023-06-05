63c3ad606699429c1d75b270c1dac9b46c9c7f77
rherault <romain@rherault.dev>
2023-06-05 12:26:00 +0000

    feat: adding filter

--- card_test.go
+++ card_test.go
@@ -61,3 +61,30 @@ func TestSort(t *testing.T) {
 		t.Error("Expected Ace of Spades as first card. Received: ", cards[0])
 	}
 }
+
+func TestJokers(t *testing.T) {
+	cards := New(Jokers(3))
+	count := 0
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count++
+		}
+	}
+
+	if count != 3 {
+		t.Error("Expected 3 Jokers, received: ", count)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	filter := func(card Card) bool {
+		return card.Rank == Two || card.Rank == Three
+	}
+
+	cards := New(Filter(filter))
+	for _, c := range cards {
+		if c.Rank == Two || c.Rank == Three {
+			t.Error("Expected to filter out Twos and Threes.")
+		}
+	}
+}
