e522f71bea258fbb3d45d66fa49f45b77db229c8
rherault <romain@rherault.dev>
2023-06-05 09:23:46 +0000

    feat: adding base structure files

---
+++ card.go
@@ -0,0 +1,44 @@
+package deck
+
+import "fmt"
+
+type Suit uint8
+
+const (
+	Spade Suit = iota
+	Diamond
+	Club
+	Heart
+	Joker
+)
+
+type Rank uint8
+
+const (
+	Ace Rank = iota + 1
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+type Card struct {
+	Suit
+	Rank
+}
+
+func (c Card) String() string {
+	if c.Suit == Joker {
+		return c.Suit.String()
+	}
+
+	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
+}
